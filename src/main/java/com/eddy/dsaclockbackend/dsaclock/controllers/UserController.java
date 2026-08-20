aefe43b9bf379ba6b2f1e654a46b30937ca29be5
atanu <atanuc2005@gmail.com>
2026-08-20 20:04:41 +0530

    feat: add update and delete mapping for users

--- src/main/java/com/eddy/dsaclockbackend/dsaclock/controllers/UserController.java
+++ src/main/java/com/eddy/dsaclockbackend/dsaclock/controllers/UserController.java
@@ -36,4 +36,36 @@ public class UserController {
     public Users setUser(@RequestBody Users user) {
         return userService.setUser(user);
     }
+
+    //update user if exists
+    @PutMapping("/{id}")
+    public ResponseEntity<Users> updateUser(
+            @PathVariable Long id,
+            @RequestBody Users user) {
+
+        Optional<Users> thisUser = userService.getUser(id);
+
+        if (thisUser.isPresent()) {
+            Users existingUser = thisUser.get();
+
+            existingUser.setUsername(user.getUsername());
+            existingUser.setEmail(user.getEmail());
+
+            return ResponseEntity.ok(userService.setUser(existingUser));
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+    //delete user
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Users> deleteUser(@PathVariable Long id) {
+        Optional<Users> thisUser = userService.getUser(id);
+
+        if(thisUser.isPresent()) {
+            userService.deleteUser(id);
+            return ResponseEntity.ok(thisUser.get());
+        }else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
--- src/main/java/com/eddy/dsaclockbackend/dsaclock/services/UserService.java
+++ src/main/java/com/eddy/dsaclockbackend/dsaclock/services/UserService.java
@@ -34,5 +34,5 @@ public class UserService {
     public Optional<Users> updateUser(Long id) { return userRepo.findById(id); }
 
     //delete user
-    public void deleteUser(Long id) {userRepo.deleteById(id); }
+    public Users deleteUser(Long id) {userRepo.deleteById(id); }
 }
