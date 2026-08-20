a25bfdedd18587983be3d8436a0ea948d0e7faf6
atanu <atanuc2005@gmail.com>
2026-08-20 20:56:02 +0530

    feat: add problem service

---
+++ src/main/java/com/eddy/dsaclockbackend/dsaclock/services/ProblemService.java
@@ -0,0 +1,41 @@
+package com.eddy.dsaclockbackend.dsaclock.services;
+
+import com.eddy.dsaclockbackend.dsaclock.entities.Problems;
+import com.eddy.dsaclockbackend.dsaclock.repos.ProblemRepo;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProblemService {
+    ProblemRepo problemRepo;
+    public ProblemService(ProblemRepo problemRepo) {  //repository instance constructor
+        this.problemRepo = problemRepo;
+    }
+
+    //return all problems in database
+    public List<Problems> getProblem() {
+        return problemRepo.findAll();
+    }
+
+    //return single problem with id
+    public Optional<Problems> getProblem(Long problem_id) {
+        return problemRepo.findById(problem_id);
+    }
+
+    //add new problem
+    public void addProblem(Problems problem) {
+        problemRepo.save(problem);
+    }
+
+    //update existing problem
+    public void updateProblem(Problems problem) {
+        problemRepo.save(problem);
+    }
+
+    //delete problem by id
+    public void deleteProblem(Long problem_id) {
+        problemRepo.deleteById(problem_id);
+    }
+}
