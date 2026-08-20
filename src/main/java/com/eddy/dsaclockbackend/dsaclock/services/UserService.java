dc0e0d1e0d24a7a1e3ccf8d04299794e5202d438
atanu <atanuc2005@gmail.com>
2026-08-20 19:20:54 +0530

    feat: add update and delete user methods

--- src/main/java/com/eddy/dsaclockbackend/dsaclock/services/UserService.java
+++ src/main/java/com/eddy/dsaclockbackend/dsaclock/services/UserService.java
@@ -15,15 +15,24 @@ public class UserService {
         this.userRepo = userRepo;
     }
 
+    //return all users
     public List<Users> getUser() {  //find all student
         return userRepo.findAll();
     }
 
+    //return a single user with id
     public Optional<Users> getUser(Long id) { //find user by id
         return userRepo.findById(id);
     }
 
+    //add new user
     public Users setUser(Users user) { //add new user
         return userRepo.save(user);
     }
+
+    //update a user data
+    public Users updateUser(Users user) { return userRepo.save(user); }
+
+    //delete user
+    public void deleteUser(Long id) {userRepo.deleteById(id); }
 }
