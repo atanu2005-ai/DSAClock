f2159db8bef99dbec644e59a83d2794f9b2fb78d
atanu <atanuc2005@gmail.com>
2026-08-20 20:54:28 +0530

    feat: add problems entity

---
+++ src/main/java/com/eddy/dsaclockbackend/dsaclock/entities/Problems.java
@@ -0,0 +1,81 @@
+package com.eddy.dsaclockbackend.dsaclock.entities;
+
+import jakarta.persistence.*;
+import org.apache.catalina.User;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "Problems")
+public class Problems {
+    //problem id
+    @Id
+    @Column(nullable = false)
+    private Long problem_id;
+
+    //problem title
+    private String problem_title;
+
+    //problem description
+    private String problem_desc;
+
+    //problem difficulty
+    private String problem_diff;
+
+    //solved date
+    private LocalDate solved_date;
+
+    //User relation with problem
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private Users user;
+
+    public Long getProblem_id() {
+        return problem_id;
+    }
+
+    public String getProblem_title() {
+        return problem_title;
+    }
+
+    public String getProblem_desc() {
+        return problem_desc;
+    }
+
+    public String getProblem_diff() {
+        return problem_diff;
+    }
+
+    public LocalDate getSolved_date() {
+        return solved_date;
+    }
+
+    public void setProblem_id(Long problem_id) {
+        this.problem_id = problem_id;
+    }
+
+    public void setProblem_title(String problem_title) {
+        this.problem_title = problem_title;
+    }
+
+    public void setProblem_desc(String problem_desc) {
+        this.problem_desc = problem_desc;
+    }
+
+    public void setProblem_diff(String problem_diff) {
+        this.problem_diff = problem_diff;
+    }
+
+    public void setSolved_date(LocalDate solved_date) {
+        this.solved_date = solved_date;
+    }
+
+    public void setUser(Users user) {
+        this.user = user;
+    }
+
+    public Users getUser() {
+        return user;
+    }
+
+}
