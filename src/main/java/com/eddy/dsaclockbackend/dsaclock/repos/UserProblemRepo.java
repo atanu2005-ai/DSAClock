cc9e4d46c70e9660d8bb34fd8fcb1ea53b056257
atanu <atanuc2005@gmail.com>
2026-08-22 19:13:59 +0530

    feat: add a feature to find all the user problems which belongs to a particular user

--- src/main/java/com/eddy/dsaclockbackend/dsaclock/repos/UserProblemRepo.java
+++ src/main/java/com/eddy/dsaclockbackend/dsaclock/repos/UserProblemRepo.java
@@ -3,5 +3,10 @@ package com.eddy.dsaclockbackend.dsaclock.repos;
 import com.eddy.dsaclockbackend.dsaclock.entities.UserProblems;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserProblemRepo extends JpaRepository<UserProblems, Long> {
+
+    //to find all user problems with relation of the current user
+    List<UserProblems> findByUserId(Long userId);
 }
