de7630f5f09203455a00cff802fbeb3de93180b6
atanu <atanuc2005@gmail.com>
2026-08-30 22:48:42 +0530

    feat: implement UserDetailsService

---
+++ src/main/java/com/eddy/dsaclockbackend/dsaclock/config/MyUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.eddy.dsaclockbackend.dsaclock.config;
+
+import com.eddy.dsaclockbackend.dsaclock.entities.Users;
+import com.eddy.dsaclockbackend.dsaclock.repos.UserRepo;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    //user repo reference
+    private final UserRepo userRepo;
+
+    public MyUserDetailsService(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Users thisUser = userRepo.findByEmail(username) //creating new user object if exists
+                .orElseThrow(() -> new UsernameNotFoundException("User not found")); //otherwise throw exception
+
+        //extracting user's username, password from user object
+        return User
+                .withUsername(thisUser.getUsername())
+                .password(thisUser.getPassword())
+                .build();
+    }
+}
