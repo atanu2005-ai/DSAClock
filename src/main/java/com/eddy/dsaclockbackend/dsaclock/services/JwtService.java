80713d2946811a5f327527bed9421621da2902ca
atanu <atanuc2005@gmail.com>
2026-08-31 01:22:54 +0530

    feat: add JwtService to generate jwt token using secret key

---
+++ src/main/java/com/eddy/dsaclockbackend/dsaclock/services/JwtService.java
@@ -0,0 +1,29 @@
+package com.eddy.dsaclockbackend.dsaclock.services;
+
+import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    //secret key field
+    private SecretKey key = Jwts.SIG.HS256.key().build();
+
+    //method to generate jwt token
+    public String generateToken(String email) {
+        Date now = new Date();
+        Date expiry = new Date(now.getTime() + 30 * 60 * 1000);
+        return Jwts.builder()
+                .subject(email) //subject of the token
+                .signWith(key) //signature key
+                .issuedAt(now) //creation date
+                .expiration(expiry) //expiry date
+                .compact();
+    }
+}
