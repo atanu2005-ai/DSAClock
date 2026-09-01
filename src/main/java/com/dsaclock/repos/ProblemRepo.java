686f8fad7cc415ba8dd3ba9c6e929a8c7bfdf886
atanu <atanuc2005@gmail.com>
2026-09-01 21:48:31 +0530

    update: add Optional Problems type method named findByProblemId to to get the problem object if exista otherwise empty object

--- src/main/java/com/dsaclock/repos/ProblemRepo.java
+++ src/main/java/com/dsaclock/repos/ProblemRepo.java
@@ -3,5 +3,9 @@ package com.dsaclock.repos;
 import com.dsaclock.entities.Problems;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProblemRepo extends JpaRepository<Problems, Long> {
+
+    Optional<Problems> findByProblemId(Long problemId); //return problem with id
 }
