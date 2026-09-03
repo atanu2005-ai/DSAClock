b3f4b012d2c9af3d51a929f2b6acd1eb98dc81cc
atanu <atanuc2005@gmail.com>
2026-09-03 22:50:11 +0530

    fix(LoginRequest): remove mapping and auuthentication part from here

--- src/main/java/com/dsaclock/dto/LoginRequest.java
+++ src/main/java/com/dsaclock/dto/LoginRequest.java
@@ -1,32 +1,9 @@
 package com.dsaclock.dto;
 
-import com.dsaclock.services.JwtService;
-import org.springframework.security.core.Authentication;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api")
 public class LoginRequest {
 
     private String email;
     private String password;
-
-    //authentication manager reference
-    private final AuthenticationManager authenticationManager;
-
-    //jwt service reference
-    private final JwtService jwtService;
-
-    public LoginRequest(AuthenticationManager authenticationManager, JwtService jwtService) {
-        this.authenticationManager = authenticationManager;
-        this.jwtService = jwtService;
-    }
 
     public String getEmail() {
         return email;
@@ -44,19 +21,4 @@ public class LoginRequest {
         this.password = password;
     }
 
-    @PostMapping("/login") //login mapping
-    public ResponseEntity<?> login(@RequestBody LoginRequest request) {
-
-        UsernamePasswordAuthenticationToken token =
-                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword());
-
-        //passing the token to manager to create an authenticated object if user gets authenticated
-        //otherwise throws exception
-        Authentication authentication = authenticationManager.authenticate(token);
-
-        String jwt = jwtService.generateToken(request.getEmail()); //generation token using request body email
-
-        return ResponseEntity.ok(jwt);
-
-    }
 }
