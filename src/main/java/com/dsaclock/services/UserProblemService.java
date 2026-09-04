15fc6a2f2178ea1b0781e1bea596a96f4279e138
atanu <atanuc2005@gmail.com>
2026-09-04 10:22:44 +0530

    feat(UserProblemService): add revision date and count login for a user problem

--- src/main/java/com/dsaclock/services/UserProblemService.java
+++ src/main/java/com/dsaclock/services/UserProblemService.java
@@ -3,15 +3,13 @@ package com.dsaclock.services;
 import com.dsaclock.entities.Problems;
 import com.dsaclock.entities.UserProblems;
 import com.dsaclock.entities.Users;
-import com.dsaclock.exceptions.ProblemNotFoundException;
-import com.dsaclock.exceptions.UserNotFoundException;
 import com.dsaclock.exceptions.UserProblemAlreadyExistsException;
 import com.dsaclock.exceptions.UserProblemNotFoundException;
 import com.dsaclock.repos.UserProblemRepo;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class UserProblemService {
@@ -54,8 +52,38 @@ public class UserProblemService {
         //set the user and problem reference to the user problem
         userProblem.setUser(user);
         userProblem.setProblem(problem);
+        userProblem.setRevision_count(0);
+
+        LocalDate today = LocalDate.now(); //current date
+
+        userProblem.setNext_revision_date(today.plusDays(2)); // revision after 2 days for first time adding
 
         return userProblemRepo.save(userProblem);
+    }
+
+    //update user problem (after a revision)
+    public UserProblems updateUserProblem(Long userId, Long problemId) {
+        UserProblems userProblems = userProblemRepo
+                .findByUser_UserIdAndProblem_ProblemId(userId, problemId).orElseThrow(() ->
+               new UserProblemNotFoundException(
+                       "No problem with such ID found associated with the user") );
+
+        int updated_count = userProblems.getRevision_count() + 1; //updated revision count
+
+        userProblems.setRevision_count(updated_count); //update revision count
+
+        //REVISION LOGIC
+        LocalDate today = LocalDate.now(); //current date
+
+        if(updated_count == 1) {
+            userProblems.setNext_revision_date(today.plusDays(3)); //after 1 revision, next revision after 3 days
+        }else if(updated_count > 1) {
+            userProblems.setNext_revision_date(null);
+        }
+
+        userProblemRepo.save(userProblems);
+
+        return userProblems;
     }
 
     //delete a user problem object
