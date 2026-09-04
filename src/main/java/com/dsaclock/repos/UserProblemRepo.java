be791685cfaa855ce0fa1c2daafb42e86b056298
atanu <atanuc2005@gmail.com>
2026-09-04 10:20:53 +0530

    fix(UserProblemRepo): find user problem with problem id instead of user problem id

--- src/main/java/com/dsaclock/repos/UserProblemRepo.java
+++ src/main/java/com/dsaclock/repos/UserProblemRepo.java
@@ -11,7 +11,7 @@ public interface UserProblemRepo extends JpaRepository<UserProblems, Long> {
     //to find all user problems with relation of the current user
     List<UserProblems> findByUserUserId(Long userId);
 
-    Optional<UserProblems> findByUser_UserIdAndUserProblemId(Long userId, Long userProblemId);
+    Optional<UserProblems> findByUser_UserIdAndProblem_ProblemId(Long userId, Long problemId);
 
     //using unique constraint to check if this pair of user and problem exists
     boolean existsByUserUserIdAndProblemProblemId(Long userId, Long problemId);
