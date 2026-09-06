7966bd71dbccac30eff657abf8ad689cc3777ca3
atanu <atanuc2005@gmail.com>
2026-09-06 13:28:05 +0530

    update(SecurityConfig): add OAuth2Login layer in security filter Chain

--- src/main/java/com/dsaclock/config/SecurityConfig.java
+++ src/main/java/com/dsaclock/config/SecurityConfig.java
@@ -5,11 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -18,8 +20,12 @@ public class SecurityConfig {
     //jwt filter reference
     private final JwtFilter jwtFilter;
 
-    public SecurityConfig(JwtFilter jwtFilter) {
+    //auth success handler reference
+    private final AuthenticationSuccessHandler oauthsuccessHandler;
+
+    public SecurityConfig(JwtFilter jwtFilter, AuthenticationSuccessHandler oauthsuccessHandler) {
         this.jwtFilter = jwtFilter;
+        this.oauthsuccessHandler = oauthsuccessHandler;
     }
 
 
@@ -41,6 +47,8 @@ public class SecurityConfig {
             //AUTHENTICATED ENDPOINTS
             auth.anyRequest().authenticated();
         })
+                .oauth2Login(oauth ->
+                        oauth.successHandler(oauthsuccessHandler))
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class) //goes through jwt filter first
                 .build();
     }
