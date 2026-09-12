9837378f06e9cb3546f9c9490cc53c391152102b
atanu <atanuc2005@gmail.com>
2026-09-13 04:00:59 +0530

    feat: add problem description for problem details, add search feature for problems page

--- frontend/src/pages/ProblemDetails.jsx
+++ frontend/src/pages/ProblemDetails.jsx
@@ -1,10 +1,12 @@
 import {useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
+import {useNavigate} from "react-router-dom";
 
 function ProblemDetails() {
 
     const {problemId} = useParams()
     const [problem, setProblem] = useState(null)
+    const navigate = useNavigate()
 
     useEffect(() => {
         fetch(`http://localhost:8080/api/problems/${problemId}`)
@@ -12,13 +14,50 @@ function ProblemDetails() {
             .then(data => setProblem(data))
     }, [problemId])
 
+    //for thr problem description
+    const [description, setDescription] = useState("")
+
+    useEffect(() => {
+        fetch(`http://localhost:8080/api/problems/${problemId}/details`)
+            .then(response => response.text())
+            .then(data => setDescription(data))
+    }, [problemId])
+
+    //for handling add button
+    const handleAdd = () => {
+        fetch(`http://localhost:8080/api/problems/${problemId}/add`, {
+            method: "POST",
+            headers: {
+                "Authorization": `Bearer ${localStorage.getItem("token")}`
+            }
+        })
+            .then(async response => {
+                if(response.ok) {
+                    navigate("/my-problems");
+                    return;
+                }
+
+                if(response.status === 409) {
+                    alert("problem already added")
+                    return;
+                }
+
+                throw new Error("Failed to add problem")
+            })
+    }
+
     return (
         <main className={"problem-details"}>
-            <h1>
-                {problemId} : {problem?.problem_title}
-            </h1>
 
-            <button>Add</button>
+            <h1>{problem?.problem_title}</h1>
+
+            <div className={"problem-description"}
+                 dangerouslySetInnerHTML={{__html:description}} />
+
+            <button className={"problem-details-buttons"} onClick={handleAdd}>
+                Add
+            </button>
+
         </main>
     )
 }
--- frontend/src/pages/Problems.jsx
+++ frontend/src/pages/Problems.jsx
@@ -1,9 +1,11 @@
-import { useEffect, useState } from "react";
+import {use, useEffect, useState} from "react";
 import { Link } from "react-router-dom";
 
 function Problems() {
 
     const [problems, setProblems] = useState([]);
+    const [searchId, setSearchId] = useState("");
+    const [searchResult, setSearchResult] = useState(null);
 
     useEffect(() => {
 
@@ -26,26 +28,82 @@ function Problems() {
 
     }, []);
 
+    //search handler
+    const handleSearch = () => {
+        const target = Number(searchId)
+
+        let left = 0;
+        let right = problems.length - 1;
+
+        while(left <= right) {
+            const mid = Math.floor((left + right) / 2);
+            const id = problems[mid].problemId;
+
+            if(id === target) {
+                setSearchResult(problems[mid]);
+                return;
+            }
+
+            if(id < target) {
+                left = mid + 1;
+            }else {
+                right = mid - 1;
+            }
+        }
+        setSearchResult(null)
+    }
+
+
     return (
         <main className="problems-page">
             <h1>Problems</h1>
 
+            <div className={"search-container"}>
+                <input
+                    type="number"
+                    value={searchId}
+                    onChange={(e) =>
+                        setSearchId(e.target.value)}
+                    placeholder="Search by problem ID"
+                />
+
+                <button onClick={handleSearch}>
+                    Search
+                </button>
+            </div>
+
             <div className="problem-list">
-                {problems.map(problem => (
+                {searchResult ? (
                     <Link
-                        to={`/problems/${problem.problemId}`}
+                        to={`/problems/${searchResult.problemId}`}
                         className="problem-card"
-                        key={problem.problemId}
+                        key={searchResult.problemId}
                     >
+                        <span className="problem-title">
+                            {searchResult.problemId} : {searchResult.problem_title}
+                        </span>
+
+                        <span className="problem-difficulty">
+                            {searchResult.problem_diff}
+                        </span>
+                    </Link>
+                ) : (
+                    problems.map(problem => (
+                            <Link
+                                to={`/problems/${problem.problemId}`}
+                                className="problem-card"
+                                key={problem.problemId}
+                            >
                         <span className="problem-title">
                             {problem.problemId} : {problem.problem_title}
                         </span>
 
-                        <span className="problem-difficulty">
+                                <span className="problem-difficulty">
                             {problem.problem_diff}
                         </span>
-                    </Link>
-                ))}
+                            </Link>
+                        ))
+                )}
             </div>
         </main>
     );
