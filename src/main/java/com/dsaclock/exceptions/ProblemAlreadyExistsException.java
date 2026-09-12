c09169a434990814a20cfed21f54b866ecb2e7e4
atanu <atanuc2005@gmail.com>
2026-09-13 04:06:09 +0530

    fix(ProblemAlreadyExistsException): exception now return 409 http response for http conflict

--- src/main/java/com/dsaclock/exceptions/ProblemAlreadyExistsException.java
+++ src/main/java/com/dsaclock/exceptions/ProblemAlreadyExistsException.java
@@ -1,5 +1,9 @@
 package com.dsaclock.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
 public class ProblemAlreadyExistsException extends RuntimeException {
 
     public ProblemAlreadyExistsException(String message) {
