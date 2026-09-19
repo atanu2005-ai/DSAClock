43fefe93581f00dba7f295611b33bc0f84ec537d
atanu <atanuc2005@gmail.com>
2026-09-19 22:09:19 +0530

    feat: add heatmap, ui changes

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 
 function Profile() {
     const [user, setUser] = useState(null);
+    const [activity, setActivity] = useState([]);
     const navigate = useNavigate();
 
     function handleLogout() {
@@ -15,7 +16,6 @@ function Profile() {
     useEffect(() => {
 
         const token = localStorage.getItem('token');
-        console.log(token);
         if (token) {
             fetch('http://localhost:8080/api/users/me', {
                 headers: {
@@ -25,9 +25,60 @@ function Profile() {
             .then(response => response.json())
             .then(data => {setUser(data)})
             .catch(error => console.error('Error fetching user profile:', error));
+        }else {
+            navigate("/login")
         }
 
     }, []);
+
+    //state for user activity
+    useEffect(() => {
+        fetch("http://localhost:8080/api/users/me/activity", {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`
+            }
+        })
+            .then(response => response.json())
+            .then(data => setActivity(data))
+            .catch(error => console.error("Error fetching activity:", error));
+    }, []);
+
+    //lookup object
+    const activityMap = {};
+
+    activity.forEach(item => {
+        activityMap[item.date] = item.revisionCount;
+    });
+
+    const today = new Date();
+    const startDate = new Date(today);
+    startDate.setFullYear(today.getFullYear() - 1);
+    console.log("start date: ", startDate);
+    console.log("today: ", today);
+
+    //now all 365 days
+    const dates = [];
+    for(let date = new Date(startDate); date <= today; date.setDate(date.getDate() + 1)) {
+        dates.push(new Date(date));
+    }
+
+    const heatmapData = dates.map(date => {
+        const dateKey = date.toISOString().split("T")[0];
+
+        return {
+            date: dateKey,
+            revisionCount: activityMap[dateKey] || 0
+        };
+    })
+
+    //heatmap intensity logic
+    const getIntensity = (count) => {
+        if(count === 0) return "level-0";
+        if(count === 1) return "level-1";
+        if(count <= 3) return "level-2"
+        return "level-4";
+    }
+
 
     return (
         <div className={"profile-page"}>
@@ -48,29 +99,55 @@ function Profile() {
                 <div className="revision-progress">
                     <h2>Revision Progress</h2>
 
+                    <div className={"heatmap-container"}>
+                        <div className="heatmap">
+                            {heatmapData.map(day => (
+                                <div
+                                    key={day.date}
+                                    className= {`heatmap-cell ${getIntensity(day.revisionCount)}`}
+                                    title={`${day.date}: ${day.revisionCount} revisions`}
+                                >
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+
                     <div className={"revision-count"}>
                         {user?.revised} / {user?.totalProblemsSolved}
-                        <p>problems solved at least once</p>
+                        <p>problems revised at least once</p>
                     </div>
 
-                    <div className={"revision-percentage"}>
-                        {user?.revisedPercentage}%
-                    </div>
+                    <div className="progress-ring">
+                        <svg width="140" height="140">
+                            <circle
+                                className="progress-ring-bg"
+                                cx="70"
+                                cy="70"
+                                r="60"
+                            />
 
-                    <div className="revision-bar">
-                        <div
-                            className="revision-bar-fill"
-                            style={{
-                                width: `${user?.revisedPercentage ?? 0}%`
-                            }}
-                        />
+                            <circle
+                                className="progress-ring-fill"
+                                cx="70"
+                                cy="70"
+                                r="60"
+                                style={{
+                                    strokeDashoffset:
+                                        377 - (377 * (user?.revisedPercentage ?? 0)) / 100
+                                }}
+                            />
+                        </svg>
+
+                        <div className="progress-text">
+                            {(user?.revisedPercentage ?? 0).toFixed(1)}%
+                        </div>
                     </div>
                 </div>
             </div>
 
-                <button className={"logout-button"} onClick={handleLogout}>
-                    Logout
-                </button>
+            <button className={"logout-button"} onClick={handleLogout}>
+                Logout
+            </button>
         </div>
     );
 }
