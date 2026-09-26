04cd9f677bb1e36fc46c332582a9bf80d7bf11c3
atanu <atanuc2005@gmail.com>
2026-09-26 19:08:50 +0530

    feat(UserProfileResponse): add current and max streak attributes in profile response dto

--- src/main/java/com/dsaclock/dto/UserProfileResponse.java
+++ src/main/java/com/dsaclock/dto/UserProfileResponse.java
@@ -3,12 +3,22 @@ package com.dsaclock.dto;
 public class UserProfileResponse {
 
     private Long id;
+
     private String username;
+
     private String email;
+
     private int totalProblemsSolved;
+
     private int totalRevisions;
+
     private int revised;
+
     private double revisedPercentage;
+
+    private int currentStreak;
+
+    private int maxStreak;
 
     public Long getId() {
         return id;
@@ -20,6 +30,22 @@ public class UserProfileResponse {
 
     public double getRevisedPercentage() {
         return revisedPercentage;
+    }
+
+    public int getCurrentStreak() {
+        return currentStreak;
+    }
+
+    public void setCurrentStreak(int currentStreak) {
+        this.currentStreak = currentStreak;
+    }
+
+    public int getMaxStreak() {
+        return maxStreak;
+    }
+
+    public void setMaxStreak(int maxStreak) {
+        this.maxStreak = maxStreak;
     }
 
     public void setRevisedPercentage(double revisedPercentage) {
